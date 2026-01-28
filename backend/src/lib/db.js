c1477bbdb27f6035d90ba55e0bd223efad293b11
A V K Bhavan Surya <avkbsurya@gmail.com>
2026-01-28 14:51:31 +0530

    Welcome Email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
+import { ENV } from './env.js';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    if(!process.env.MONGO_URI) throw new Error("MONGO_URI is not set in environment variables");
+    const {MONGO_URI} = ENV;
+    if(!MONGO_URI) throw new Error("MONGO_URI is not set in environment variables");
+    const conn = await mongoose.connect(ENV.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   }catch (error) {
     console.error(`Error: ${error.message}`);
