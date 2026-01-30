03036de8242eb8966517a40472b95677e4ad801c
A V K Bhavan Surya <avkbsurya@gmail.com>
2026-01-30 05:36:25 +0530

    Implemented Real Time messages and cleared code structure

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
-import { ENV } from './env.js';
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const {MONGO_URI} = ENV;
-    if(!MONGO_URI) throw new Error("MONGO_URI is not set in environment variables");
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
     const conn = await mongoose.connect(ENV.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  }catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // 1 STATUS FAILURE, 0 STATUS SUCCESS
-  } 
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
+  }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import { getAllContacts,getMessagesByUserId,sendMessage, getChatPartners } from '../controllers/message.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.use(arcjetProtection,protectRoute);
-
-//the middleware execute in order - so requests het rate-limited and authenticated first
-// this is actually more efficient than adding the middleware to each route individually
+// the middlewares execute in order - so requests get rate-limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
 router.get("/:id", getMessagesByUserId);
 router.post("/send/:id", sendMessage);
 
-export default router;  
+export default router;
